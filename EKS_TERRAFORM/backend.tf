3e4dc5d093a4a7003c7589c8550a1a53fd7c46a1
KSVN CHINMAYI <kotrachinmayi117@gmail.com>
2025-08-05 12:52:10 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "2048-bkt" # Replace with your actual S3 bucket name
+    bucket = "mybkt-2048" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
